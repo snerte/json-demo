812c5643632671ec1ce1d6f833898872bd6dd184
Maarten Göertz <Maarten.Goertz@centric.eu>
2021-03-19 15:06:00 +0100

    Generating JSON output

--- run.js
+++ run.js
@@ -5,13 +5,18 @@ const fs = require('fs');
 const directoryPath = path.join(__dirname, 'svg');
 //passsing directoryPath and callback function
 fs.readdir(directoryPath, function (err, files) {
+
     //handling error
     if (err) {
         return console.log('Unable to scan directory: ' + err);
     }
+
     //listing all files using forEach
     files.forEach(function (file) {
         // Do whatever you want to do with the file
         console.log(file);
     });
+
+    let data = JSON.stringify(files);
+    fs.writeFileSync('fails.json', data);
 });
